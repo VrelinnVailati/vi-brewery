d9b9dcbb769ae450df2ce15d8af12ef28fe4b1d3
Vi Vailati <ambarlife845@gmail.com>
2023-11-08 17:22:03 -0600

    Change BeerController to work as a REST controller

--- src/main/java/dev/vailati/vibrewery/controller/BeerController.java
+++ src/main/java/dev/vailati/vibrewery/controller/BeerController.java
@@ -4,19 +4,30 @@ import dev.vailati.vibrewery.model.Beer;
 import dev.vailati.vibrewery.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id) {
+    @GetMapping
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+    @GetMapping("{beerId}")
+    public Beer getBeerById(@PathVariable UUID beerId) {
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
