70784bd00b9d5a6e584f7e2de739a1f30461ebc4
Vi Vailati <ambarlife845@gmail.com>
2023-11-08 15:18:55 -0600

    Initial commit

---
+++ src/main/java/dev/vailati/vibrewery/ViBreweryApplication.java
@@ -0,0 +1,13 @@
+package dev.vailati.vibrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vailati/vibrewery/ViBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vailati.vibrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
