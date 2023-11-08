c0e28f4adfa6a134b73ab1e14e0d4a1aae09034a
Vi Vailati <ambarlife845@gmail.com>
2023-11-08 17:21:14 -0600

    Change beer service and service implementation to return mock values

--- src/main/java/dev/vailati/vibrewery/services/BeerService.java
+++ src/main/java/dev/vailati/vibrewery/services/BeerService.java
@@ -2,8 +2,11 @@ package dev.vailati.vibrewery.services;
 
 import dev.vailati.vibrewery.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
