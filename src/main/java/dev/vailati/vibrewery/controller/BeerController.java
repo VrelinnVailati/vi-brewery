cdb61f3fb9578e0b8b816f17d1ad29c57a1d8dbd
Vi Vailati <ambarlife845@gmail.com>
2023-11-08 18:05:36 -0600

    Add POST endpoint to create beers

--- src/main/java/dev/vailati/vibrewery/controller/BeerController.java
+++ src/main/java/dev/vailati/vibrewery/controller/BeerController.java
@@ -4,10 +4,9 @@ import dev.vailati.vibrewery.model.Beer;
 import dev.vailati.vibrewery.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +17,13 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity<Void> createBeer(@RequestBody Beer beer) {
+        beerService.saveBeer(beer);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
     @GetMapping
     public List<Beer> listBeers() {
