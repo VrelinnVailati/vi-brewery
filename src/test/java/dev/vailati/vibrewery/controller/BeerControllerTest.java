ce03d961cf336fb2f74dd1f26ec514e19b4acc00
Vi Vailati <ambarlife845@gmail.com>
2023-11-14 15:46:05 -0600

    Add DELETE test for /beer

--- src/test/java/dev/vailati/vibrewery/controller/BeerControllerTest.java
+++ src/test/java/dev/vailati/vibrewery/controller/BeerControllerTest.java
@@ -7,6 +7,7 @@ import dev.vailati.vibrewery.services.BeerService;
 import dev.vailati.vibrewery.services.BeerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -41,31 +45,54 @@ public class BeerControllerTest {
     }
 
     @Test
-    void testUpdateBeerById() throws Exception {
+    void testDeleteBeerById() throws Exception {
+        // Arrange
         Beer beer = beerServiceImpl.listBeers().get(0);
 
+        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+
+        // Act
+        ResultActions response = mockMvc.perform(delete("/api/v1/beer/" + beer.getId())
+                .accept(MediaType.APPLICATION_JSON));
+
+        // Assert
+        response.andExpect(status().isOk());
+        verify(beerService).deleteBeerById(uuidArgumentCaptor.capture());
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+    }
+
+    @Test
+    void testUpdateBeerById() throws Exception {
+        // Arrange
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        //Act
         ResultActions response = mockMvc.perform(put("/api/v1/beer/" + beer.getId())
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beer)));
 
+        // Assert
         response.andExpect(status().isOk());
         verify(beerService).updateBeerById(eq(beer.getId()), any(Beer.class));
     }
 
     @Test
     void testCreateNewBeer() throws Exception {
+        // Arrange
         Beer beer = beerServiceImpl.listBeers().get(0);
         beer.setVersion(null);
         beer.setId(null);
 
         given(beerService.saveBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
 
+        // Act
         ResultActions response = mockMvc.perform(post("/api/v1/beer")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beer)));
 
+        // Assert
         response.andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
     }
@@ -83,14 +110,17 @@ public class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
+        // Arrange
         Beer testBeer = beerServiceImpl.listBeers().get(0);
 
         given(beerService.getBeerById(testBeer.getId()))
                 .willReturn(testBeer);
 
+        // Act
         ResultActions response = mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
                 .accept(MediaType.APPLICATION_JSON));
 
+        // Assert
         response.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
