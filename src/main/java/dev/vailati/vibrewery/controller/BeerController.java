e48b70f9880817030d7fe8fb3587e513734b9151
Vi Vailati <ambarlife845@gmail.com>
2023-11-17 15:02:59 -0600

    Add exception handler for BeerController

--- src/main/java/dev/vailati/vibrewery/controller/BeerController.java
+++ src/main/java/dev/vailati/vibrewery/controller/BeerController.java
@@ -59,6 +59,11 @@ public class BeerController {
         return beerService.listBeers();
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Void> handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
     @GetMapping(BEER_PATH_ID)
     public Beer getBeerById(@PathVariable UUID beerId) {
         log.debug("Get Beer by Id - in controller");
