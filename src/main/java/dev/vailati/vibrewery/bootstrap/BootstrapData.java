b4354f6cf5f011049db8edc6de0b315c908471ce
Vi Vailati <ambarlife845@gmail.com>
2023-11-18 18:20:15 -0600

    Create BootstrapData class

---
+++ src/main/java/dev/vailati/vibrewery/bootstrap/BootstrapData.java
@@ -0,0 +1,81 @@
+package dev.vailati.vibrewery.bootstrap;
+
+import dev.vailati.vibrewery.entities.Beer;
+import dev.vailati.vibrewery.entities.Customer;
+import dev.vailati.vibrewery.model.BeerStyle;
+import dev.vailati.vibrewery.repositories.BeerRepository;
+import dev.vailati.vibrewery.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) {
+        loadBeerData();
+        loadCustomerData();
+    }
+
+    private void loadBeerData() {
+        if(beerRepository.count() == 0) {
+            Beer beer1 = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("123456")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(122)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer2 = Beer.builder()
+                    .beerName("Crank")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12356222")
+                    .price(new BigDecimal("11.99"))
+                    .quantityOnHand(392)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer3 = Beer.builder()
+                    .beerName("Sunshine City")
+                    .beerStyle(BeerStyle.IPA)
+                    .upc("12356")
+                    .price(new BigDecimal("13.99"))
+                    .quantityOnHand(144)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            beerRepository.save(beer1);
+            beerRepository.save(beer2);
+            beerRepository.save(beer3);
+        }
+    }
+
+    private void loadCustomerData() {
+        if(customerRepository.count() == 0) {
+            Customer customer1 = Customer.builder()
+                    .customerName("John Doe")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            Customer customer2 = Customer.builder()
+                    .customerName("Jane Smith")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            customerRepository.save(customer1);
+            customerRepository.save(customer2);
+        }
+    }
+}
