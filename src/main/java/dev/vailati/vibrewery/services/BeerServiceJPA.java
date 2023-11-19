2ae458ec030614e52771a49513263709b16c95f1
Vi Vailati <ambarlife845@gmail.com>
2023-11-18 19:35:41 -0600

    Implement list and get operations for /beer and add tests

--- src/main/java/dev/vailati/vibrewery/services/BeerServiceJPA.java
+++ src/main/java/dev/vailati/vibrewery/services/BeerServiceJPA.java
@@ -20,12 +20,15 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> listBeers() {
-        return null;
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .toList();
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return beerRepository.findById(id).map(beerMapper::beerToBeerDto);
     }
 
     @Override
---
+++ src/test/java/dev/vailati/vibrewery/controller/BeerControllerIT.java
@@ -0,0 +1,57 @@
+package dev.vailati.vibrewery.controller;
+
+import dev.vailati.vibrewery.entities.Beer;
+import dev.vailati.vibrewery.model.BeerDTO;
+import dev.vailati.vibrewery.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    private BeerController beerController;
+
+    @Autowired
+    private BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyListBeers() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testGetBeerById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+        assertThat(dto.getId()).isEqualTo(beer.getId());
+    }
+
+    @Test
+    void testGetBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> beerController.getBeerById(UUID.randomUUID()));
+    }
+}
