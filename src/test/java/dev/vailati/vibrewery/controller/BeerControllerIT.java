ce4184121f7565e4da67aede347b273b8a20d03b
Vi Vailati <ambarlife845@gmail.com>
2023-11-19 19:52:46 -0600

    Implement integration tests for saving and updating a Beer

--- src/test/java/dev/vailati/vibrewery/controller/BeerControllerIT.java
+++ src/test/java/dev/vailati/vibrewery/controller/BeerControllerIT.java
@@ -1,14 +1,18 @@
 package dev.vailati.vibrewery.controller;
 
 import dev.vailati.vibrewery.entities.Beer;
+import dev.vailati.vibrewery.mappers.BeerMapper;
 import dev.vailati.vibrewery.model.BeerDTO;
 import dev.vailati.vibrewery.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,6 +26,9 @@ class BeerControllerIT {
 
     @Autowired
     private BeerRepository beerRepository;
+
+    @Autowired
+    private BeerMapper beerMapper;
 
     @Test
     void testListBeers() {
@@ -53,5 +60,52 @@ class BeerControllerIT {
     @Test
     void testGetBeerIdNotFound() {
         assertThrows(NotFoundException.class, () -> beerController.getBeerById(UUID.randomUUID()));
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveBeer() {
+        String beerName = "New Beer";
+
+        BeerDTO beerDTO = BeerDTO.builder()
+                .beerName(beerName).build();
+
+        ResponseEntity<BeerDTO> responseEntity = beerController.createBeer(beerDTO);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+        String locationUri = responseEntity.getHeaders().getLocation().getPath();
+        UUID savedUUID = UUID.fromString(locationUri.split("/")[4]);
+
+        Beer savedBeer = beerRepository.findById(savedUUID).get();
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getBeerName()).isEqualTo(beerName);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testUpdateBeerById() {
+        Beer beer = beerRepository.findAll().get(0);
+        final String beerName = "Updated Name";
+
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity<Void> responseEntity = beerController.updateBeerById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
+
+    @Test
+    void testUpdateNotFoundBeer() {
+        assertThrows(NotFoundException.class, () -> beerController.updateBeerById(UUID.randomUUID(), BeerDTO.builder().build()));
     }
 }
