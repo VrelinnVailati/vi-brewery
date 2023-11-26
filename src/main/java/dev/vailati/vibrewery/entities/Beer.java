619e2844f742d65b578a06194c7defccdb277016
Vi Vailati <ambarlife845@gmail.com>
2023-11-25 18:16:29 -0600

    Add validation for the Beer entity

--- src/main/java/dev/vailati/vibrewery/entities/Beer.java
+++ src/main/java/dev/vailati/vibrewery/entities/Beer.java
@@ -2,6 +2,8 @@ package dev.vailati.vibrewery.entities;
 
 import dev.vailati.vibrewery.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -23,10 +25,14 @@ public class Beer {
     @Version
     private Integer version;
 
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/dev/vailati/vibrewery/repositories/BeerRepositoryTest.java
+++ src/test/java/dev/vailati/vibrewery/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package dev.vailati.vibrewery.repositories;
 
 import dev.vailati.vibrewery.entities.Beer;
+import dev.vailati.vibrewery.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("New Beer")
+                        .price(new BigDecimal("12.99"))
+                        .upc("123123123")
+                        .beerStyle(BeerStyle.PALE_ALE)
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
