d9769958894562da7bc1d9ca694c4e4ecd03813a
Vi Vailati <ambarlife845@gmail.com>
2023-11-25 18:24:18 -0600

    Add validation for database constraints

--- src/test/java/dev/vailati/vibrewery/repositories/BeerRepositoryTest.java
+++ src/test/java/dev/vailati/vibrewery/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package dev.vailati.vibrewery.repositories;
 
 import dev.vailati.vibrewery.entities.Beer;
 import dev.vailati.vibrewery.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -29,5 +31,19 @@ class BeerRepositoryTest {
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void testSaveBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("New Beer that is definitely too long so I hopefully get it right")
+                    .price(new BigDecimal("12.99"))
+                    .upc("123123123")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .build());
+
+            beerRepository.flush();
+        });
     }
 }
